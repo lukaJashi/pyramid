91760c45b060d57d0154fc5caad6a237d3e71f1d
lukaJashi <110906385+lukaJashi@users.noreply.github.com>
2022-10-15 13:45:38 +0400

    Add files via upload

---
+++ pyramid/пирамида2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пирамида2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int x, y, z;
+                Console.WriteLine("pick a number");
+               
+                z = Convert.ToInt32(Console.ReadLine());
+
+                for (x = 0; x < z; x++)
+                {
+
+                    for (y = 1; y <= z - x; y++)
+                        Console.Write(" ");
+                    for (y = 1; y <= 2 * x - 1; y++)
+                        Console.Write("*");
+                    Console.Write("\n");
+                }
+
+                }
+            }
+    }  
+}
